e71db45cb3cd59bed413522d1b1941d5b385337f
Shubhanshu Singh <shubhanshusingh2451@gmail.com>
2025-07-02 18:11:30 +0530

    latest version updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 android {
     namespace = "com.shubhans.taskmanager"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.shubhans.taskmanager"
